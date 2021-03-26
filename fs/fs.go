8082fd216ff061a47d0cffc943864eb899a12c3c
Peter Huncar <peter.huncar@innovatrics.com>
2021-03-27 00:48:00 +0100

    fs dir scan comences

--- fs/fs.go
+++ fs/fs.go
@@ -1,1 +1,17 @@
 package fs
+
+import (
+	"time"
+)
+
+type FileDesc struct {
+	FilePath string
+	FileSize uint64
+	Modified time.Time
+	Sha1     []byte
+	Weak     []uint32
+}
+
+func GetList(path string) ([]*FileDesc, error) {
+	return nil, nil
+}
