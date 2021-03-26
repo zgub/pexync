01aa5f506afee5771832a9c07055d7fba0d3dcab
Peter Huncar <peter.huncar@innovatrics.com>
2021-03-26 23:37:09 +0100

    hashlist created

--- core/core.go
+++ core/core.go
@@ -1,1 +1,48 @@
 package core
+
+import (
+	"bufio"
+	"hash/adler32"
+	"io"
+	"os"
+)
+
+type Block struct {
+	Offset uint64
+	Data   []byte
+}
+
+func GetChecksums(f *os.File, blockSize int) ([]uint32, error) {
+
+	r := bufio.NewReader(f)
+	buffer := make([]byte, blockSize)
+	fileInfo, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	size := fileInfo.Size()
+	l := size / int64(blockSize)
+	if (size % int64(blockSize)) != 0 {
+		l++
+	}
+
+	hashList := make([]uint32, l)
+
+	for i := 0; ; i++ {
+		//n, err := r.Read(buffer[:cap(buffer)])
+		//buf = buf[:n]
+		n, err := r.Read(buffer)
+		if n == 0 {
+			if err == nil {
+				continue
+			}
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		sum := adler32.Checksum(buffer)
+		hashList[i] = sum
+	}
+	return hashList, nil
+}
