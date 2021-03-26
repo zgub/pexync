2a3cc56d0add1887e13aeab18d048abcbaf59c77
Peter Huncar <peter.huncar@innovatrics.com>
2021-03-26 23:53:09 +0100

    checsums tested

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/zgub/pexync
 
 go 1.16
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/rs/zerolog v1.21.0
+	github.com/spf13/viper v1.7.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
 
-func Main() {
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	"github.com/zgub/pexync/core"
+)
+
+func main() {
+
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("CGL")
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if viper.GetBool("debug") {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	// TODO #1
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	fmt.Println("Hello world")
+	f, err := os.Open("main.go")
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("error")
+	}
+	sums, err := core.GetChecksums(f, 700)
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("error")
+	}
+	spew.Dump(sums)
 }
