3cdbed92dfb722813cd1320f2eda2fec7abf0e4b
Peter Huncar <peter.huncar@innovatrics.com>
2021-04-04 01:08:22 +0200

    rolling

--- workers/reader.go
+++ workers/reader.go
@@ -1,6 +1,7 @@
 package workers
 
 import (
+	"bufio"
 	"context"
 	"io"
 	"sync"
@@ -9,21 +10,29 @@ import (
 )
 
 type RollReader struct {
-	reader    *io.SectionReader
-	receiver  <-chan []*core.Message
-	blockSize int
-	buffer    []byte
+	reader     *io.SectionReader
+	receiver   <-chan []*core.Message
+	blockSize  int
+	sendBuffer []byte
 }
 
 func NewRollReader(ctx context.Context, wg *sync.WaitGroup, blockSize int, sr *io.SectionReader, receiver <-chan []*core.Message) *RollReader {
 	return &RollReader{
-		reader:    sr,
-		receiver:  receiver,
-		blockSize: blockSize,
-		buffer:    make([]byte, blockSize),
+		reader:     sr,
+		receiver:   receiver,
+		blockSize:  blockSize,
+		sendBuffer: make([]byte, blockSize),
 	}
 }
 
 func (rr *RollReader) Start() {
+	br := bufio.NewReader(rr.reader)
+	buff := make([]byte, rr.blockSize)
+	n, err := io.ReadFull(br, buff)
+	if n == 0 && err == io.EOF {
+		core.Fatality(err)
+	}
+	rh := core.Pour()
+	rh.Write(buff)
 
 }
