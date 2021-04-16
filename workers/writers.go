0f1e63ecca63d106b0b4ecf93856970eba2d4c24
Peter Huncar <peter.huncar@innovatrics.com>
2021-04-16 22:41:45 +0200

    initial writer work

--- workers/writers.go
+++ workers/writers.go
@@ -1,1 +1,21 @@
 package workers
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/zgub/pexync/core"
+)
+
+type FileWriter struct {
+	ctx      context.Context
+	inbox    <-chan *core.Message
+	senderID uuid.UUID
+}
+
+func NewFileWriter(ctx context.Context, inbox <-chan *core.Message) FileWriter {
+	return FileWriter{
+		ctx:   ctx,
+		inbox: inbox,
+	}
+}
