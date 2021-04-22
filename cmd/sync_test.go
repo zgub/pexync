c3b37e0a7abb767078793cfc15c75e80e0181f2b
Peter Huncar <peter.huncar@innovatrics.com>
2021-04-22 15:25:23 +0200

    new test package, moved test file creation

--- cmd/sync_test.go
+++ cmd/sync_test.go
@@ -1,33 +1,15 @@
 package cmd
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/md5"
-	"fmt"
 	"io"
-	"math/rand"
 	"os"
 	"path/filepath"
 	"testing"
-	"unicode/utf8"
 
 	"github.com/spf13/viper"
 )
-
-type testFileType int
-
-const (
-	AABBCC testFileType = iota
-	BBCCDD
-	AACCEE
-)
-
-var testFileTypes = [...]string{
-	"AABBCC",
-	"BBCCDD",
-	"AACCEE",
-}
 
 const (
 	srcD = "../test/"
@@ -36,74 +18,6 @@ const (
 
 func (t testFileType) String() string {
 	return testFileTypes[t]
-}
-
-func createTestFile(dir string, blockSize, blockCount int, t testFileType) (string, error) {
-
-	path := fmt.Sprintf(dir+"/%dx%d-test-data-%s", blockCount, blockSize, t.String())
-	f, err := os.Create(path)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-
-	bw := bufio.NewWriter(io.Writer(f))
-	switch t {
-	case AABBCC:
-		rn := rune('a')
-		buf := make([]byte, utf8.UTFMax)
-		for block := 0; block < blockCount; block++ {
-			for byte := 0; byte < blockSize; byte++ {
-				n := utf8.EncodeRune(buf, rn)
-				buf = buf[:n]
-				_, err = bw.Write(buf)
-				if err != nil {
-					return "", err
-				}
-			}
-			rn++
-		}
-	case BBCCDD:
-		rn := rune('b')
-		buf := make([]byte, utf8.UTFMax)
-		for block := 0; block < blockCount; block++ {
-			for byte := 0; byte < blockSize; byte++ {
-				n := utf8.EncodeRune(buf, rn)
-				buf = buf[:n]
-				_, err = bw.Write(buf)
-				if err != nil {
-					return "", err
-				}
-			}
-			rn++
-		}
-	case AACCEE:
-		rn := rune('a')
-		buf := make([]byte, utf8.UTFMax)
-		for block := 0; block < blockCount; block++ {
-			for byte := 0; byte < blockSize; byte++ {
-				n := utf8.EncodeRune(buf, rn)
-				buf = buf[:n]
-				_, err = bw.Write(buf)
-				if err != nil {
-					return "", err
-				}
-			}
-			rn++
-			rn++
-		}
-	}
-
-	for i := 0; i < rand.Intn(10); i++ {
-		_, err = bw.Write([]byte("END"))
-		if err != nil {
-			return "", err
-		}
-	}
-
-	bw.Flush()
-	f.Sync()
-	return path, nil
 }
 
 func TestMissingLocalSync(t *testing.T) {
