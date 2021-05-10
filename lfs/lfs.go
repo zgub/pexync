20261d3b779d3cf2fa2d05d914d9f6ca0bda8fa4
Peter Huncar <peter.huncar@innovatrics.com>
2021-05-11 00:22:53 +0200

    win switch

--- lfs/lfs.go
+++ lfs/lfs.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"syscall"
+	//"syscall"
 	"time"
 
 	"github.com/pkg/errors"
@@ -330,7 +330,7 @@ func ParseDir(walkDir string) ([]*FileDesc, error) {
 			return errors.Wrap(err, "file stat info failed")
 		}
 
-		stat := info.Sys().(*syscall.Stat_t)
+		//stat := info.Sys().(*syscall.Stat_t)
 
 		relPath, err := filepath.Rel(walkDir, path)
 		if err != nil {
@@ -354,8 +354,8 @@ func ParseDir(walkDir string) ([]*FileDesc, error) {
 				FileSize: uint64(info.Size()),
 				Modified: info.ModTime(),
 				Mode:     info.Mode(),
-				Uid:      stat.Uid,
-				Gid:      stat.Gid,
+				//Uid:      stat.Uid,
+				//Gid:      stat.Gid,
 			}
 
 			list = append(list, fileDesc)
