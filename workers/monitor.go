6a49bbc5abde43aad30086235c3ca0ae4dbd2624
Peter Huncar <peter.huncar@innovatrics.com>
2021-05-20 00:27:29 +0200

    cp generates two events...

--- workers/monitor.go
+++ workers/monitor.go
@@ -1,10 +1,10 @@
 package workers
 
 import (
-	"bytes"
 	"fmt"
 	"path/filepath"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/fsnotify/fsnotify"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
@@ -52,70 +52,86 @@ func NewMonitor(rrCh, brCh chan *core.Message, watchList []*lfs.FileDesc) (Monit
 }
 
 func (m Monitor) eval(event fsnotify.Event) {
-	if event.Op&fsnotify.Write == fsnotify.Write {
-		log.Info().
-			Str("path", event.Name).
-			Msg("WRITE - event detected")
-		// event file descriptor
-		efd, err := lfs.Scan(event.Name)
-		if err != nil {
-			log.Error().
-				Err(err).
-				Msg("file stat error")
-			// let's ignore errors, too may untested edge cases
-			return
-		}
-		if fd, ok := m.watchMap[event.Name]; ok {
-			// write event on a known file
-			if fd.FileSize == efd.FileSize {
-				// size did not change, let's then calculate SHA1 digests
-				efd.Sha1, err = efd.GetSha1()
-				if err != nil {
-					log.Error().
-						Err(err).
-						Str("filename", event.Name).
-						Msg("failed to calulate SHA1 digest")
-					return
-				}
-				if bytes.Equal(efd.Sha1, fd.Sha1) {
-					// digests are equal, ignore
-					log.Info().
-						Str("filename", event.Name).
-						Msg("WRITE - file has not changed")
-					return
+	log.Debug().
+		Str("event", event.Name).
+		Str("operation", event.Op.String()).
+		Msg("event")
+	efd, err := lfs.Scan(event.Name)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Msg("file stat error")
+		// let's ignore errors, too may untested edge cases
+		return
+	}
+	spew.Dump(efd)
+	/*
+		if event.Op&fsnotify.Write == fsnotify.Write {
+			log.Info().
+				Str("path", event.Name).
+				Msg("WRITE - event detected")
+			// event file descriptor
+			efd, err := lfs.Scan(event.Name)
+			if err != nil {
+				log.Error().
+					Err(err).
+					Msg("file stat error")
+				// let's ignore errors, too may untested edge cases
+				return
+			}
+			if fd, ok := m.watchMap[event.Name]; ok {
+				// write event on a known file
+				if fd.FileSize == efd.FileSize {
+					// size did not change, let's then calculate SHA1 digests
+					efd.Sha1, err = efd.GetSha1()
+					if err != nil {
+						log.Error().
+							Err(err).
+							Str("filename", event.Name).
+							Msg("failed to calulate SHA1 digest")
+						return
+					}
+					if bytes.Equal(efd.Sha1, fd.Sha1) {
+						// digests are equal, ignore
+						log.Info().
+							Str("filename", event.Name).
+							Msg("WRITE - file has not changed")
+						return
+					} else {
+						// digests are not equal - send changes
+						log.Info().
+							Str("filename", event.Name).
+							Msg("WRITE - file has changed")
+					}
 				} else {
-					// digests are not equal - send changes
+					// sizes are different - send changes
 					log.Info().
 						Str("filename", event.Name).
-						Msg("WRITE - file has changed")
+						Msg("WRITE - file size change")
 				}
 			} else {
-				// sizes are different - send changes
-				log.Info().
+				log.Warn().
 					Str("filename", event.Name).
-					Msg("WRITE - file size change")
+					Msg("WRITE - event on unknown file, ignoring")
+				return
 			}
-		} else {
-			log.Warn().
-				Str("filename", event.Name).
-				Msg("WRITE - event on unknown file, ignoring")
-			return
 		}
-	}
-	if event.Op&fsnotify.Remove == fsnotify.Remove {
-		log.Info().
-			Str("path", event.Name).
-			Msg("REMOVE - event detected, ignoring")
-	}
-	if event.Op&fsnotify.Chmod == fsnotify.Chmod {
-		log.Info().
-			Str("path", event.Name).
-			Msg("CHMOD - event detected, ignoring")
-	}
-	if event.Op&fsnotify.Create == fsnotify.Create {
-		log.Info().
-			Str("path", event.Name).
-			Msg("CRETAE - event detected")
+		if event.Op&fsnotify.Remove == fsnotify.Remove {
+			log.Info().
+				Str("path", event.Name).
+				Msg("REMOVE - event detected, ignoring")
+		}
+		if event.Op&fsnotify.Chmod == fsnotify.Chmod {
+			log.Info().
+				Str("path", event.Name).
+				Msg("CHMOD - event detected, ignoring")
+		}
+		if event.Op&fsnotify.Create == fsnotify.Create {
+			log.Info().
+				Str("path", event.Name).
+				Msg("CREATE - event detected")
+	*/
+	/*
 		efd, err := lfs.Scan(event.Name)
 		if err != nil {
 			log.Error().
@@ -124,23 +140,28 @@ func (m Monitor) eval(event fsnotify.Event) {
 			// let's ignore errors, too may untested edge cases
 			return
 		}
+		spew.Dump(efd)
 		// to calculate checksum we need to determine the block size first
-		efd.SetBlockSize()
-		err = core.AddChecksums(efd)
-		if err != nil {
-			log.Error().
-				Err(err).
-				Msg("failed to calculate checksums")
-			return
+
+			efd.SetBlockSize()
+			err = core.AddChecksums(efd)
+			if err != nil {
+				log.Error().
+					Err(err).
+					Msg("failed to calculate checksums")
+				return
+			}
+			m.idx++
+			efd.Idx = m.idx
+	*/
+	/*
 		}
-		m.idx++
-		efd.Idx = m.idx
-	}
-	if event.Op&fsnotify.Rename == fsnotify.Rename {
-		log.Info().
-			Str("path", event.Name).
-			Msg("RENAME - event detected")
-	}
+		if event.Op&fsnotify.Rename == fsnotify.Rename {
+			log.Info().
+				Str("path", event.Name).
+				Msg("RENAME - event detected")
+		}
+	*/
 }
 
 func (m Monitor) Start() error {
